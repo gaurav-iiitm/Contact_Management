bb04394634fdf890b8c20c58c34e06f740463131
Gaurav <gauravyadavabviiitm@gmail.com>
2021-10-06 01:06:12 +0530

    First_Configuration_setup

---
+++ src/main/java/com/gaurav/contact_app/ContactAppApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.contact_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ContactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAppApplication.class, args);
+		System.out.println("Contact App is started");
+	}
+
+}
